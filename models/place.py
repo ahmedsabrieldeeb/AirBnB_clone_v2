d6be20cf7666db3940d8daf3c90b6acbad401df3
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-01-01 16:47:56 -0800

    adjust place.py

--- models/place.py
+++ models/place.py
@@ -31,13 +31,13 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(128), nullable=False)
+    description = Column(String(128), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="all, delete")
     amenities = relationship(
             "Amenity", secondary='place_amenity',

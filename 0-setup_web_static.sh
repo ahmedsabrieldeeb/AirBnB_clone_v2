dd71a01fc9fddf2003b048d38d09af6334241471
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-02-11 04:51:47 +0200

    update the script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,7 +20,9 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sudo sed -i 's@location / {@location /hbnb_static/ {\n    alias /data/web_static/current/;\n    index index.html;\n}\n\nlocation / {@' /etc/nginx/sites-available/default
+if ! grep -q 'location \/hbnb_static\/ {' /etc/nginx/sites-available/default; then
+    sudo sed -i '$s@}$@\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}\n}@' /etc/nginx/sites-available/default
+fi
 
 # Restart Nginx
 sudo service nginx restart

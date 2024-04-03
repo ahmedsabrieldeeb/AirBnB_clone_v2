16d950be708ecd8c7446985915f80a7d586834bc
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-03 12:41:26 +0200

    Update do_deploy function to handle archive extraction and create symbolic link

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ from os.path import exists
 env.hosts = ['ubuntu@100.26.11.29', 'ubuntu@18.204.20.248']
 env.key_filename = "/home/ahmedisdeep/.ssh/school"
 
+
 @task
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder"""
@@ -20,6 +21,7 @@ def do_pack():
     except Exception as e:
         return None
 
+
 @task
 def do_deploy(archive_path):
     """Distributes an archive to web servers"""
@@ -29,13 +31,17 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, '/tmp/')
-        archive_filename = archive_path.split('/')[-1].split('.')[0]
-        release_folder = '/data/web_static/releases/{}'.format(archive_filename)
+        archive_filename = archive_path.split('/')[-1] \
+            .split('.')[0]
+        release_folder = '/data/web_static/releases/{}' \
+            .format(archive_filename)
         run('sudo mkdir -p {}'.format(release_folder))
-        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(archive_filename, release_folder))
+        run('sudo tar -xzf /tmp/{}.tgz -C {}'
+            .format(archive_filename, release_folder))
         run('sudo rm /tmp/{}.tgz'.format(archive_filename))
         run('sudo rm /data/web_static/current')
-        run('sudo ln -s {}/web_static /data/web_static/current'.format(release_folder))
+        run('sudo ln -s {}/web_static /data/web_static/current'
+            .format(release_folder))
 
         return True
     except Exception as e:
